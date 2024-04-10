f7478ef7a56ca8ef526db1141813ef7a9e18c0cb
Joshua Bass <joshuabass77@gmail.com>
2024-04-10 17:21:32 -0500

    worked questions 1a - 1D

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -12,17 +12,42 @@ for (let i=0; i<4; i++) {
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
-console.log('count from 0 to 5');
+
+for (let i=0; i<5; i++){ 
+  console.log ("for in loop o to 5 count", i )
+
+}
+console.log('count from 0 to 5' );
+
+// Answer:  you would need to change the index max to 5, i<5
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
+
+for (let i=3; i<5; i++){
+  console.log(`for in loop 3 to 5 count `, i);
+
+}
+
+
 console.log('count from 3 to 5');
+
+// Answer:  index needs to be set to 3 and max needs to be set 5 , i=3 && i<5
+
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
+
+
+
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
+
+for (let i=5; i>0; i-- ){
+  console.log(`backwards count from 5`, i);
+}
+
 console.log('STRETCH: countdown from 5 to 0');
 
 
