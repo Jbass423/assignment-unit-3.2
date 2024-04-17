2c15a8d15511016cb5007f56118524ece1d5d311
Joshua Bass <joshuabass77@gmail.com>
2024-04-17 15:42:27 -0500

    fixed numbers and placment of  i

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,18 +13,18 @@ for (let i = 0; i < 4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i <= 5; i++) {
   console.log("for in loop o to 5 count", i)
 
 }
 console.log('count from 0 to 5');
 
-// Answer:  you would need to change the index max to 5, i<5
+// Answer:  you would need to change the index max to 5, i<=5
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 
-for (let i = 3; i < 5; i++) {
+for (let i = 3; i <=5; i++) {
   console.log(`for in loop 3 to 5 count `, i);
 
 }
@@ -39,7 +39,7 @@ console.log('count from 3 to 5');
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 
-for (let i = 2; i < 10; i += 2) {
+for (let i = 2; i < 11; i += 2) {
   console.log("count even numbers", i);
 }
 
@@ -98,8 +98,9 @@ let i = 0;
 const max = 10;
 
 while (i < 5) {
-  i++
+ 
   console.log(`count 0 to 5 loop:`, i)
+  i++
 
 };
 
